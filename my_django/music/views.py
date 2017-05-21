205c2f4455a6da4662af4790e8b4186bc58d51da
nook <nookdanger@gmail.com>
2017-05-21 22:06:16 +0700

    test

--- my_django/music/views.py
+++ my_django/music/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-    return HttpResponse("<h1> Hello world</h1>");
+from .models import Album
+from django.template import loader
 
+def index(request):
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+    	'all_albums':all_albums,
+    }
+    return HttpResponse(template.render(context,request))
+
+
+def detail(request,album_id):
+	return HttpResponse("<h2>Details for Album id: " +str(album_id)+" </h2>");
